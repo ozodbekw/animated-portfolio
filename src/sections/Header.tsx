dadaaa41bdef242fb9a30e9d80b3fb1f5b20f485
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-05-18 15:55:17 +0500

    feat: implement Hero section with animations and responsive design

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center relative top-3 ">
+    <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/50 rounded-full bg-white/10 backdrop-blur ">
         <a href="#" className="nav-item">
           Home
@@ -13,7 +13,7 @@ export const Header = () => {
         </a>
         <a
           href="#"
-          className="nav-item bg-white text-gray-900 hover:bg-white-70 hover:text-gray-900"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
         >
           Contact
         </a>
