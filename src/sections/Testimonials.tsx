f2aa1f91b8693e2b8a12ffdbd557482281c6d2f9
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-05-23 18:13:19 +0500

    feat: enhance Testimonials section layout and styling for improved presentation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -50,18 +50,29 @@ export const TestimonialsSection = () => {
         work."
         title="How Clients Say About Me"
       />
-      <div>
-        {testimonials.map((testimonial) => (
-          <Card key={testimonial.name}>
-            <Image
-              src={testimonial.avatar}
-              alt={testimonial.name + " avatar"}
-            />
-            <div>{testimonial.name}</div>
-            <div>{testimonial.position}</div>
-            <p>{testimonial.text}</p>
-          </Card>
-        ))}
+      <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="flex-none gap-8 flex">
+          {testimonials.map((testimonial) => (
+            <Card key={testimonial.name} className="max-w-xs">
+              <div className="flex gap-4 items-center ">
+                <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                  <Image
+                    src={testimonial.avatar}
+                    alt={testimonial.name + " avatar"}
+                    className="max-h-full"
+                  />
+                </div>
+                <div>
+                  <div className="font-semibold ">{testimonial.name}</div>
+                  <div className="text-sm text-white/40">
+                    {testimonial.position}
+                  </div>
+                </div>
+              </div>
+              <p className="mt-4 text-sm">{testimonial.text}</p>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
