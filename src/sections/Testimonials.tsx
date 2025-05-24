be560e227f3875687892a9ddfc970342f56153aa
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-05-24 17:49:43 +0500

    feat: enhance Testimonials section with responsive design and improved styling

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,17 +43,20 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-24">
       <SectionHeader
         eyebrow="Happy clients"
         describtion="Don't just take my word for it. See what clients have to say about my
         work."
         title="How Clients Say About Me"
       />
-      <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
         <div className="flex-none gap-8 flex">
           {testimonials.map((testimonial) => (
-            <Card key={testimonial.name} className="max-w-xs">
+            <Card
+              key={testimonial.name}
+              className="max-w-xs md:p-8 md:max-w-md"
+            >
               <div className="flex gap-4 items-center ">
                 <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
                   <Image
@@ -69,7 +72,9 @@ export const TestimonialsSection = () => {
                   </div>
                 </div>
               </div>
-              <p className="mt-4 text-sm">{testimonial.text}</p>
+              <p className="mt-4 md:mt-6 text-sm md:text-base">
+                {testimonial.text}
+              </p>
             </Card>
           ))}
         </div>
