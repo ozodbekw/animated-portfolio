57d9fc906c84e7f8f448c3626faa3d75dc9f1541
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-05-27 14:37:58 +0500

    feat: enhance layout and styling of About and Testimonials sections

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -93,7 +93,7 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <div className="pb-20">
+    <div className="pb-20 lg:py-28">
       <div className="container">
         <SectionHeader
           describtion="Learn more about who I am, what I do and what inspares me."
@@ -101,67 +101,70 @@ export const AboutSection = () => {
           title="A Glimpse Into My World"
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
-            <CardHeader
-              title="My Reads"
-              describtion=" Explore the books shaping my perspectivies."
-            />
-            <div className="w-40 mx-auto mt-8 -mb-">
-              <Image src={BookImage} alt="Book cover" />
-            </div>
-          </Card>
-          <Card className="h-[320px] p-0">
-            <CardHeader
-              className="px-6 py-6"
-              title="My Toolbook"
-              describtion="Explore the technologies and tools I use to create digital
-                experience."
-            />
-            <ToolbosItems className="mt-6" items={toolboxItems} />
-            <ToolbosItems
-              className="mt-6"
-              items={toolboxItems}
-              itemsWrapperClassName="-translate-x-1/2"
-            />
-          </Card>
-          <Card className="h-[320px] p-0 flex flex-col">
-            <CardHeader
-              title="Beyong the Code"
-              describtion="Explore my interests and hobbies beyong the degital relm."
-              className="px-6 py-6"
-            />
-            <div className="relative flex-1">
-              {hobbies.map((hobby) => (
-                <div
-                  key={hobby.title}
-                  className="inline-flex absolute items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5"
-                  style={{
-                    left: hobby.left,
-                    top: hobby.top,
-                  }}
-                >
-                  <span className="font-medium text-gray-950">
-                    {hobby.title}
-                  </span>
-                  <span>{hobby.emoji}</span>
-                </div>
-              ))}
-            </div>
-          </Card>
-          <Card className="p-0 h-[320px] relative">
-            <Image
-              src={MapImage}
-              alt="map image"
-              className="h-full w-full object-cover object-left-top"
-            />
-            <div className="absolute rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
-              <Image
-                className="size-20"
-                src={SmileMemoji}
-                alt="smiling memoji"
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                describtion="Explore the books shaping my perspectivies."
               />
-            </div>
-          </Card>
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={BookImage} alt="Book cover" />
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbook"
+                describtion="Explore the technologies and tools I use to create digital
+                experience."
+              />
+              <ToolbosItems className="" items={toolboxItems} />
+              <ToolbosItems
+                className="mt-6"
+                items={toolboxItems}
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8 lg:grid-cols-3">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyong the Code"
+                describtion="Explore my interests and hobbies beyong the degital relm."
+                className="px-6 py-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex absolute items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="p-0 h-[320px] relative md:col-span-2 lg:col-span-1">
+              <Image
+                src={MapImage}
+                alt="map image"
+                className="h-full w-full object-cover object-left-top"
+              />
+              <div className="absolute rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <Image
+                  className="size-20"
+                  src={SmileMemoji}
+                  alt="smiling memoji"
+                />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
