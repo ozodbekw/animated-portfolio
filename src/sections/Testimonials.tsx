bf95ebd3921a3e7f1d0dddc5a3c1fd2588c2d56f
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-06-02 22:24:49 +0500

    feat: update Testimonials section layout and enhance Card hover effect

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -50,12 +50,12 @@ export const TestimonialsSection = () => {
         work."
         title="How Clients Say About Me"
       />
-      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
         <div className="flex-none gap-8 flex">
           {testimonials.map((testimonial) => (
             <Card
               key={testimonial.name}
-              className="max-w-xs md:p-8 p-8 md:max-w-md"
+              className="max-w-xs md:p-8 p-8 md:max-w-md hover:-rotate-3 transition duration-300"
             >
               <div className="flex gap-4 items-center ">
                 <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
